14a75c5d8a9f546a3e270ea90a62bf7c7480c582
Maria <km.nvxh222@gmail.com>
2020-07-04 15:44:26 +0300

    add devises imprint

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.Secure
 import android.util.Log
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Long.toHexString
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mInterstitialAd: InterstitialAd
+    lateinit var android_id: String
 
     companion object {
         const val AD_UNIT_ID = "ca-app-pub-2579532894113456/9413517755"
@@ -53,7 +60,49 @@ class MainActivity : AppCompatActivity() {
         } else {
             Log.d("TAG", "The interstitial wasn't loaded yet.")
         } }
+
+        imprint.setOnClickListener {
+            getDevisesImprint(this)
+        }
     }
 
 
-}
+    @SuppressLint("HardwareIds")
+    fun getDevisesImprint(context: Context): String {
+        //Android Secure ID
+        android_id = Secure.getString(contentResolver, Secure.ANDROID_ID);
+        Log.d("MainActivity", "getDevisesImprint ${android_id}")
+
+        //Android gsf ID
+        lateinit var hexString: String
+        val sUri: Uri = Uri.parse("content://com.google.android.gsf.gservices")
+        try {
+            val query: Cursor = context.contentResolver
+                .query(sUri, null, null, arrayOf("android_id"), null)
+                ?: return "Not found"
+            if (!query.moveToFirst() || query.columnCount < 2) {
+                query.close()
+                return "Not found"
+            }
+            hexString = toHexString(query.getString(1).toLong())
+            query.close()
+            hexString.toUpperCase(Locale.ROOT).trim { it <= ' ' }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+
+        } catch (e2: Exception) {
+            e2.printStackTrace()
+
+        }
+
+        Log.d("MainActivity", "getGSFID  ${hexString}")
+
+        //Build.Fingerprint
+        val build_fingerprint = Build.FINGERPRINT
+        Log.d("MainActivity", "Build.Fingerprint  ${build_fingerprint}")
+
+        return hexString+android_id
+
+        }
+    }
+
